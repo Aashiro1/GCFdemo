f19f1a1a1ee3f3d64f018460dfb99a8aa6968a88
Aashiro1 <shakyaaashish@gmail.com>
2023-10-09 17:16:49 +0545

    new edit

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const readMoreButtons = document.querySelectorAll(".secondary-btn");
 
 readMoreButtons.forEach(function (button) {
   button.addEventListener("click", function () {
+    const container = button.closest(
+      ".our-story-container, .about-us-container"
+    );
     const paragraph = this.parentElement.querySelector(
       ".paragraph-content.extra"
     );
@@ -9,8 +12,10 @@ readMoreButtons.forEach(function (button) {
     paragraph.classList.toggle("show");
 
     if (paragraph.classList.contains("show")) {
+      container.style.height = "auto"; // Expand container to fit content
       this.textContent = "Read Less";
     } else {
+      container.style.height = "520px";
       this.textContent = "Read More";
     }
   });
