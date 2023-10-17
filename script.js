1620a928b0ad497b4c01d7d3e8f409329914cebf
Aashiro1 <shakyaaashish@gmail.com>
2023-10-17 09:57:57 +0545

    new changes

--- script.js
+++ script.js
@@ -10,16 +10,14 @@ window.addEventListener("scroll", function () {
 const burgerMenu = document.querySelector(".burger-menu");
 const navLinks = document.querySelector(".nav-links");
 
-// Function to hide the menu
 function hideMenu() {
-  navLinks.classList.remove("active");
-  burgerMenu.classList.remove("active");
+  navLinks.classList.remove("activate");
+  burgerMenu.classList.remove("activate");
 }
 
-// Toggle menu on burger menu click
 burgerMenu.addEventListener("click", function () {
-  navLinks.classList.toggle("active");
-  burgerMenu.classList.toggle("active");
+  navLinks.classList.toggle("activate");
+  burgerMenu.classList.toggle("activate");
 });
 
 const navLinksContainer = document.querySelector(".ul");
@@ -48,11 +46,45 @@ readMoreButtons.forEach(function (button) {
 
     if (paragraph.classList.contains("show")) {
       container.style.paddingBottom = "40px";
-      container.style.height = "auto"; // Expand container to fit content
+      container.style.height = "auto";
       this.textContent = "Read Less";
     } else {
       container.style.height = "520px";
       this.textContent = "Read More";
+      container.scrollIntoView({ behavior: "smooth" });
     }
   });
+});
+
+// section animation
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+// Active link
+const activePage = window.location.pathname;
+const navBarLinks = document.querySelectorAll(".nav-link");
+navBarLinks.forEach((link) => {
+  const navLinkPathname = new URL(link.href).pathname;
+
+  if (
+    (activePage === navLinkPathname && activePage !== "/index.html") ||
+    (activePage === "/index.html" &&
+      (navLinkPathname === "/" || navLinkPathname === "/index.html"))
+  ) {
+    link.classList.add("active");
+  }
 });
